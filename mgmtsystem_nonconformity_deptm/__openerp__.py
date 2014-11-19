811d7d49d5e2e0d52e1a2d6b83e40c86e48ac4ba
Loïc Faure-Lacroix <lamerstar@gmail.com>
2014-11-19 14:23:21 +0300

    Marked the module  mgmtsystem_nonconformity_deptm as installable

--- mgmtsystem_nonconformity_deptm/__openerp__.py
+++ mgmtsystem_nonconformity_deptm/__openerp__.py
@@ -26,6 +26,6 @@
     "depends": ['mgmtsystem_nonconformity', 'hr'],
     "init": [],
     "data": ['mgmtsystem_nonconformity.xml'],
-    'installable': False,
+    'installable': True,
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
